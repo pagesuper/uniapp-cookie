79d67625e914408270dc3f9de59c8cb95ce1e43e
WangJinzhong <andywang7259@163.com>
2024-12-18 19:02:36 +0800

    1.0.0 dev

--- src/api.ts
+++ src/api.ts
@@ -1,27 +1,7 @@
-declare const qq: any;
-declare const tt: any;
-declare const swan: any;
-declare const wx: any;
-declare const my: any;
+declare const uni: any;
 
 function getApi() {
-  if (typeof my !== 'undefined') {
-    my.platform = 'my';
-    return my;
-  } else if (typeof tt !== 'undefined') {
-    tt.platform = 'tt';
-    return tt;
-  } else if (typeof swan !== 'undefined') {
-    swan.platform = 'swan';
-    return swan;
-  } else if (typeof qq !== 'undefined') {
-    qq.platform = 'qq';
-    return qq;
-  } else if (typeof wx !== 'undefined') {
-    wx.platform = typeof window !== 'undefined' && typeof location !== 'undefined' ? 'h5' : 'wx';
-    return wx;
-  }
-  return { platform: 'none' };
+  return uni;
 }
 
 export default getApi();
